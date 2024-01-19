394412543087d02073b1a58f670104e1d4cb9145
Kristina Kachalkova <kachalkovakristina@mail.ru>
2024-01-19 20:30:23 +0300

    1.0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,26 @@
-import Popovers from "../components/popovers/popovers";
+import Popovers from '../components/popovers/popovers';
 
 const popovers = new Popovers();
-let activePopovers = [];
+const activePopovers = [];
 
 const btns = document.querySelectorAll('.btn');
 
-btns.forEach(btn => {
-    btn.addEventListener('click', onBtnClick);
+btns.forEach((btn) => {
+  btn.addEventListener('click', onBtnClick);
 });
 
-
 function onBtnClick(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const element = e.target;
+  const element = e.target;
 
-    const indexEl = activePopovers.findIndex(id => element.dataset.id == id);
+  const indexEl = activePopovers.findIndex((id) => element.dataset.id == id);
 
-    if (indexEl !== -1) {
-        activePopovers.splice(indexEl, 1);
-        popovers.removePopover(element.dataset.id);
-        element.dataset.id = '';
-    } else {
-        activePopovers.push(popovers.showPopover('Pop-up text', element));
-    }
+  if (indexEl !== -1) {
+    activePopovers.splice(indexEl, 1);
+    popovers.removePopover(element.dataset.id);
+    element.dataset.id = '';
+  } else {
+    activePopovers.push(popovers.showPopover('Pop-up text', element));
+  }
 }
-
-
-
